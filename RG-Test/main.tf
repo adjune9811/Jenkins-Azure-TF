66bc1be8177ff6626194d9cc08953a190b89ed40
Ajay Rawat <ajayrawat111@gmail.com>
2025-01-02 19:49:19 +0530

    add terrafrom rg file

---
+++ RG-Test/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+}
+
+
+
+provider "azurerm" {
+      features {}
+  subscription_id = "f4dc0a9c-6aa8-4b81-a3c9-92f5852b6cc4"
+}
+
+resource "azurerm_resource_group" "RG-TF" {
+  name     = "Jenkins-TF-RG"
+  location = "West Europe"
+}
